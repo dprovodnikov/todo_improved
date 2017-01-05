9308328bab202eb1c3de78d99969440d842f2758
Neest <denyska7@yandex.ru>
2017-01-05 15:21:21 +0200

    ADD: filters panel above the task list became more interactive, you can switch the filters in the dropdown menu, it pulls changes of the filter panel title and so on.

--- js/task_manager/taskmanager.vue
+++ js/task_manager/taskmanager.vue
@@ -1,32 +1,36 @@
 <template>
   
   <div class="tl-task-filter">
-    <div class="tl-filter-title tl-filter-today">Tasks for today</div>
+    <div class="tl-filter-title tl-filter-today">{{ f.active.hint }}</div>
     <div class="tl-filter-controls">
+
       <div class="tl-filter-select"
            @click="f.show = !f.show"
-           v-bind:class="{'select-open': f.show, 'select-close': !f.show}">
+           :class="{'select-open': f.show, 'select-close': !f.show}">
 
-        <div class="tl-filter-name">By date</div>
+        <div class="tl-filter-name">By {{ f.active.name }}</div>
 
         <i class="fa fa-filter tl-filter-icon"></i>
 
         <div class="tl-filter-list">
-          <li v-for="filter in f.list">
+
+          <li v-for="filter in f.list | available" @click="f.active = filter">
             <i :class="filter.icon"></i>
-            {{filter.text}}
+            {{ filter.name }}
           </li>
+
         </div>
 
       </div>
+
     </div>
   </div>
 
   <div class="tasklist-global" v-click-outside="task-unfocus">
     <task v-for="task in tasks"
-          v-on:task-remove="removeTask(task)"
-          v-bind:task="task"
-          v-bind:event-bus="eventBus">
+          :task-remove="removeTask(task)"
+          :task="task"
+          :event-bus="eventBus">
     </task>
   </div> 
 
@@ -39,30 +43,46 @@
 
   export default {
     props: ['eventBus'],
+
     components: {
       task: taskComponent
     },
+
+    filters: {
+      available: function(array) {
+        return array.filter(item => this.f.active != item);
+      }
+    },
+
     directives: {
       'click-outside': clickOutsideDirective
     },
+
     data: function() {
       return {
         tasks: taskList,
         f: {
           list: [
-           { icon: 'fa fa-flag', text: 'Priority' },
-           { icon: 'fa fa-sort-alpha-asc', text: 'Alphabet' },
-           { icon: 'fa fa-sort-amount-asc', text: 'Length' },
+            { icon: 'fa fa-calendar', name: 'date', hint: 'Tasks for today' },
+            { icon: 'fa fa-flag', name: 'priority', hint: 'High pripority first' },
+            { icon: 'fa fa-sort-alpha-asc', name: 'alphabet', hint: 'Tasks in alphabet order' },
+            { icon: 'fa fa-sort-amount-asc', name: 'length', hint: 'Longest tasks first' },
           ],
+          active: {},
           show: false,
         }
       };
     },
+
     methods: {
       removeTask: function(task) {
         this.tasks.$remove(task);
       }
     },
+
+    created: function() {
+      this.f.active = this.f.list[0];
+    }
   }
 
 </script>
