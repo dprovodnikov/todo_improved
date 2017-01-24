ea3569f823714d29096f71cdce7db8304f1a8500
Neest <denyska7@yandex.ru>
2017-01-25 00:57:43 +0200

    Resolved #28: mongoose support is added.

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import { join } from 'path';
+import bodyParser from 'body-parser';
+import session from 'express-session';
+
+import config from './config';
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.database, (err) => {
+  if(err) throw err;
+
+  console.log('mongoose connected');
+});
 
 const app = express();
 
 app.use(express.static(join(__dirname, '/public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: config.secret,
+}));
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(8080);
+app.listen(config.port, err => {
+  if(err) throw err;
+
+  console.log(`Listening on http://localhost:${config.port}/`);
+});
---
+++ config/index.js
@@ -0,0 +1,7 @@
+const config = {
+  database: 'localhost/todo-improved',
+  port: 7000,
+  secret: 'verysecret'
+}
+
+export default config;
