077645ab1b0b799701e38f02ed9229e0ac70fd65
w1reden <wireden@yandex.ru>
2017-02-22 15:19:08 +0200

    Added for #38: auth form design has become completely different

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,2 +1,15 @@
 // styles
 import styles from '../stylus/landing/main.styl'
+
+// Vues
+import authForm from './modules/auth-form/auth-form.vue';
+
+new Vue({
+
+  el: 'body',
+
+  components: {
+    'auth-form': authForm,
+  }
+
+});
---
+++ public/js/modules/auth-form/auth-form.vue
@@ -0,0 +1,53 @@
+<template>
+  
+  <div class="auth-form">
+    <div class="auth-form__track">
+
+      <div class="form-hint form-hint--left-aligned">
+        <h2 class="form-hint__h">Don't Have an account?</h2>
+        <p class="form-hint__text">Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Exercitationem eligendi repudiandae
+        </p>
+        <div class="form-hint__footer">
+          <button class="form-button">Sign up</button>
+        </div>
+      </div>
+
+      <div class="form-hint form-hint--right-aligned">
+        <h2 class="form-hint__h">Have an account?</h2>
+        <p class="form-hint__text">Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Exercitationem eligendi repudiandae
+        </p>
+        <div class="form-hint__footer">
+          <button class="form-button">Sign in</button>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="form-instance form-instance--left-aligned">
+      <form>
+        <h class="form-instance__title">Sign Up</h>
+        
+        <fieldset>
+          <input type="text" placeholder="Full name">
+          <input type="text" placeholder="E-mail">
+          <input type="password" placeholder="Password">
+        </fieldset>
+
+        <div class="form-instance__footer">
+          <a href="#" class="form-instance__link">Forgot password?</a>
+          <button class="form-button form-button--filled">Sign Up</button>
+        </div>
+
+      </form>
+    </div>
+
+    <div class="auth-form__box"></div>
+  </div>
+
+</template>
+
+<script>
+  export default {}
+</script>
