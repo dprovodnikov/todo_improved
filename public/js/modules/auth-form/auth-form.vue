9f726563fc8505e06c5fd905d39ebbd0a2680e8f
w1reden <wireden@yandex.ru>
2017-03-01 10:20:21 +0200

    Resolved #38: auth/reg form animation

--- public/js/modules/auth-form/auth-form.vue
+++ public/js/modules/auth-form/auth-form.vue
@@ -3,51 +3,118 @@
   <div class="auth-form">
     <div class="auth-form__track">
 
-      <div class="form-hint form-hint--left-aligned">
+      <div v-show="!mode" class="form-hint form-hint--left-aligned">
         <h2 class="form-hint__h">Don't Have an account?</h2>
         <p class="form-hint__text">Lorem ipsum dolor sit amet, consectetur
           adipisicing elit. Exercitationem eligendi repudiandae
         </p>
         <div class="form-hint__footer">
-          <button class="form-button">Sign up</button>
+          <button class="form-button" @click="toggleMode">Sign up</button>
         </div>
       </div>
 
-      <div class="form-hint form-hint--right-aligned">
+      <div v-show="mode" class="form-hint form-hint--right-aligned">
         <h2 class="form-hint__h">Have an account?</h2>
         <p class="form-hint__text">Lorem ipsum dolor sit amet, consectetur
           adipisicing elit. Exercitationem eligendi repudiandae
         </p>
         <div class="form-hint__footer">
-          <button class="form-button">Sign in</button>
+          <button class="form-button" @click="toggleMode">Sign in</button>
         </div>
       </div>
 
     </div>
 
-    <div class="form-instance form-instance--left-aligned">
+    <div class="form-instance form-instance--{{boxAlignClass}}-aligned">
       <form>
-        <h class="form-instance__title">Sign Up</h>
+        <h1 class="form-instance__title">{{title}}</h2>
         
         <fieldset>
-          <input type="text" placeholder="Full name">
-          <input type="text" placeholder="E-mail">
-          <input type="password" placeholder="Password">
+          <input v-if="mode" v-model="fullName" type="text" placeholder="Full name">
+          <input v-model="email" type="text" placeholder="E-mail">
+          <input v-model="password" type="password" placeholder="Password">
         </fieldset>
 
         <div class="form-instance__footer">
           <a href="#" class="form-instance__link">Forgot password?</a>
-          <button class="form-button form-button--filled">Sign Up</button>
+          <button class="form-button form-button--filled" @click.prevent="execute">{{title}}</button>
         </div>
 
       </form>
     </div>
 
-    <div class="auth-form__box"></div>
+    <div class="auth-form__box auth-form__box--{{boxAlignClass}}-aligned"></div>
   </div>
 
 </template>
 
 <script>
-  export default {}
+  export default {
+    data: function() {
+      return {
+        mode: true,
+        fullName: '',
+        password: '',
+        email: '',
+      }
+    },
+
+    methods: {
+      toggleMode: function() {
+        this.mode = !this.mode;
+        this.emptyForm();
+      },
+
+      emptyForm() {
+        this.fullName = '';
+        this.password = '';
+        this.email = '';
+      },
+
+      execute: function() {
+        if (this.mode && !this.email.trim()) {
+          return false
+        }
+
+        const { fullName, password, email } = this;
+
+        if (this.mode) {
+          console.log(`registration: ${fullName}, ${password}, ${email}`)
+        } else {
+          console.log(`authorization: ${email}, ${password}`)
+        }
+
+      },
+    },
+
+    computed: {
+      boxAlignClass: function() {
+        return this.mode ? 'left' : 'right'
+      },
+
+      title: function() {
+        return this.mode ? 'Sign Up' : 'Sign In';
+      }
+    }
+  }
 </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
